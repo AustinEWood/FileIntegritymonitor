74d5f511e78ed87c8892b408fdc126f30f403a0b
AustinEWood <Austinwoodllc@yahoo.com>
2022-01-02 22:39:56 -0500

    Added better error handling. Moved to one file. Added more comments.

--- FileIntegrityMonitor/clear.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-def clear_file():
-    log_clear = open("logfile.log", "w")
-    log_clear.close()
-    print("File has been cleared")
--- FileIntegrityMonitor/filecasket.py
+++ FileIntegrityMonitor/filecasket.py
@@ -1,6 +1,84 @@
-from hashfile import hash_file
-from clear import clear_file
-from check_hash import recheck
+import os
+import hashlib
+import logging
+import datetime
+from pathlib import Path
+
+
+# Get date and time from the current os
+datetime_class = datetime.datetime.now()
+# Get file to log information to
+logging.basicConfig(filename="logfile.log", level=logging.DEBUG)
+
+try:
+    def error_handle(errorcode):
+        if(errorcode == 1):
+            print("Invalid File")
+            if(errorcode == 2):
+                print("Did not work")
+except:
+    print("Critical error")
+
+# clear file function
+try:
+    def clear_file():  # Define function
+        # Open log file in write mode this will erase it
+        log_clear = open("logfile.log", "w")
+        log_clear.close()  # close log file
+        print("File has been cleared")
+except:
+    error_handle(2)  # if error message
+
+# Get file function
+try:
+    def get_file():  # Define function
+        name = input("File name:")  # Get file name
+        path = Path.cwd()  # Get current working directory name it path
+        # Walk from current working directory to were the file is
+        for root, dirs, files in os.walk(path):
+            if name in files:  # Check for file name in directory
+                # Get whole path of the file
+                path_to_file = (os.path.join(root, name))
+                return path_to_file
+except:
+    error_handle(2)
+
+# Hash file and save to file
+try:
+    def hash_file():
+        path_to_file = get_file()
+        with open(path_to_file, "rb") as f:
+            file_hash = hashlib.md5()
+            while chunk := f.read(8192):
+                file_hash.update(chunk)
+                (file_hash.digest())
+                print(file_hash.hexdigest())
+                logging.info(path_to_file)
+                logging.info(file_hash.hexdigest())
+                logging.info(datetime_class)
+except:
+    error_handle(2)
+
+try:
+    def recheck():
+        path_to_file = get_file()
+
+        with open(path_to_file, "rb") as f:
+            file_hash = hashlib.md5()
+            while chunk := f.read(8192):
+                file_hash.update(chunk)
+                (file_hash.digest())
+                print(file_hash.hexdigest())
+                check__hash = file_hash.hexdigest()
+
+        log_check = open("logfile.log", "r")
+        read_log = log_check.read()
+        if check__hash in read_log:
+            print("hash", file_hash.hexdigest(), "found in file")
+        else:
+            print("hash", file_hash.hexdigest(), "not found")
+except:
+    error_handle(2)
 
 
 print("hash or check or clear")
@@ -14,11 +92,6 @@ try:
     elif choice == "check":
         recheck()
     else:
-        print("That is not a choice....")
-        print("Enter hash or check or clear no spaces no upper case")
-        print("Goodbye!")
+        error_handle(1)
 except:
-    print("No file with that name...")
-    print("Check the spelling")
-    print("File type must be added as so readme.txt")
-    print("Goodbye!")
+    error_handle(1)
